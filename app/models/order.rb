670ef6ba04036f3e6f731a95a875f3d3a36d1036
wusunghan   <diadora1166@gmail.com>
2019-03-21 00:23:34 +0800

    When Order success, product reduce

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
+  after_create :sell
   has_many :order_products
+  validates_presence_of :products
   has_many :products, through: :order_products
+
+  def sell
+    products = self.products
+    products.each do |product|
+      product.sell
+      product.sold_out
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,20 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many :orders, through: :order_products
 
+
   def status
     if is_alive?
       "在庫中"
     else
       "已售完"
     end
+  end
+
+  def sell
+    self.count = self.count - 1
+  end
+
+  def sold_out
+    self.is_alive = false if self.count == 0
   end
 end
