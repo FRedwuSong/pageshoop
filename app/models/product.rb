560c12073a443a5cf1ea881cff85fd2a5106e890
wusunghan   <diadora1166@gmail.com>
2019-03-20 22:06:41 +0800

    Model/ Product : Create a method , status

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   has_many :order_products
   has_many :orders, through: :order_products
+
+  def status
+    if is_alive?
+      "在庫中"
+    else
+      "已售完"
+    end
+  end
 end
