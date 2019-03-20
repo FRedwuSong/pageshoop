370fe1a5f5addca40e7cc44ca2a51305fa74c467
wusunghan   <diadora1166@gmail.com>
2019-03-20 20:26:05 +0800

    Add product rspec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "商品(product)的規格" do
+
+    let(:product) { Product.create!( :name => "book" ) }
+
+    it "新建一個商品時，有name，預設數量==1，商品狀態為在庫中" do
+      expect(product.name).to eq("book")
+      expect(product.count) == (1)
+      expect(product.name) == ("在庫中")
+    end
+
+    it "當商品數量為零時，商品狀態為已售完" do
+      product.sell(1)
+      expect(product.status).to eq("已售完")
+    end
+  end
 end
