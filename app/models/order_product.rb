a3af4e4ebd2f6a2988c66e6489600b2d98d9fab3
wusunghan   <diadora1166@gmail.com>
2019-03-21 00:25:10 +0800

    When orders's product is not exist , the order fail

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,14 @@
 class OrderProduct < ApplicationRecord
+  validate :product_must_exist
   belongs_to :order
   belongs_to :product
+
+  private
+  def product_must_exist
+    product = Product.find(product_id)
+    if !product.is_alive?
+      order.errors.messages[:product]= ["商品庫存已⽤完, 整份訂單失敗"]
+      raise ActiveRecord::Rollback
+    end
+  end
 end
